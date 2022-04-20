af3a17a4ff8df460595afcc422fe3f55c4750378
Jose L Gonzalez C <jose.gonzalez@qrvey.com>
2022-04-20 17:55:58 -0500

    :zap: Improvement(Matematicas) Added console log and docs

--- src/matematica/suma.ts
+++ src/matematica/suma.ts
@@ -1,9 +1,13 @@
 /**
- * @version 0.0.0
- * @param num1 
- * @param num2 
+ * Returns the sum of two numbers
+ * @since 0.0
+ * @version 1.0.1
+ * @param num1 the first number
+ * @param num2 the second number
  * @returns num1 + num2
  */
 export function suma(num1 = 0, num2 = 0): number {
-    return num1 + num2;
+    const result = num1 + num2;
+    console.log(result);
+    return result;
 }
